26a4da15ce881f6dcef13cecf078c37ee19215e9
vishalBansall <165312130+vishalBansall@users.noreply.github.com>
2024-03-31 17:39:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Vishal"; //string
+let age = 22; // number
+let isLoggedIn = false; //boolean;
+
+// number --> 2 ki power 53 is range
+// bigInt --> when we require large no. above number range
+// string --> ""/ ''
+// boolean --> true/false
+// null --> its a standAlone value/ representation of an empty value
+// undefined --> variable declared without assigning value
+// symbol --> used majorly in react/ symbol is used to find uniqueness
+//object
+
+// typeof is used to find type of datatypes or variables
+//*** typeof null is --> object;
