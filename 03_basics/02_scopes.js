8332c517662870bd5ee4725a9cb566dcfd363105
vishalBansall <165312130+vishalBansall@users.noreply.github.com>
2024-04-30 19:33:53 +0000

    scopes done nested scope pending in between

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+let a = 10 // --> block scope
+const b = 20 // --> block scope
+var c = 30 //--> Functional scope [Majorly ignored]
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//***********Nested Scopes********************
+
