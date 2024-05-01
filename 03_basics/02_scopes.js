ebc856f36b648dd058069b006dd8ec7d374c2994
vishalBansall <165312130+vishalBansall@users.noreply.github.com>
2024-05-01 18:36:51 +0000

    nested scope in js done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,3 +8,27 @@ console.log(c);
 
 //***********Nested Scopes********************
 
+// --> child will have th access to parent variables but parent will not have the access to child variables.
+
+
+// ++++++++++++Interesting +++++++++++++++++++
+// --> Techniques to write functions:-
+
+//Method 1:-
+
+function addOne(num){
+    return num + 1;
+}
+
+addOne(4);
+
+
+// Method 2:-
+
+let addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);
+
+// Both the methods are same....Here this 'addTwo' variables are called as expression.
